5a3292b5dec525fef47ebe4378bbc3e96aa302e9
Ben Briant <b.briant@unsw.edu.au>
2024-03-17 20:24:40 +1100

    Formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,6 +82,38 @@ return {
 		end
 	},
 
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+			
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					svelte = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					graphql = { "prettier" },
+					lua = { "stylua" },
+					python = { "isort", "black" },
+
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = true,
+					-- timeout_ms = 500,
+				},
+			})
+		end,
+	},
+
 	-- tmux integration
 	"christoomey/vim-tmux-navigator",
 
---
+++ test.py
@@ -0,0 +1,8 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+
+
+def foo(test: int) -> float:
+    return 53
