8751a8162619da2afee00bec25639a471b364f4c
Ben Briant <b.briant@unsw.edu.au>
2024-03-17 18:47:40 +1100

    lsp crap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -48,6 +48,40 @@ return {
 	-- indentation based on current file
 	"tpope/vim-sleuth",
 
+	-- LSP stuff
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local lsp_zero = require('lsp-zero')
+
+			lsp_zero.on_attach(function(client, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({buffer = bufnr})
+			end)
+
+			local cmp = require('cmp')
+			cmp.setup({
+				mapping = {
+					['<CR>'] = cmp.mapping.confirm({ select = false }),
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				}
+			})
+
+			require('lspconfig').clangd.setup({})
+		end
+	},
+
 	-- tmux integration
 	"christoomey/vim-tmux-navigator",
 
